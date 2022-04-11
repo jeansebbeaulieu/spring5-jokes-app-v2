e8eb1e7622d2d38d6f92d901b1b3b9f8c5d80912
Jean-Sébastien Beaulieu <jsbeaulieu87@gmail.com>
2022-04-11 15:31:29 -0400

    Moved ChuckNorrisQuotes bean into @Configuration

--- src/main/java/com/jeansebbeaulieu/jokes/services/JokesServiceImpl.java
+++ src/main/java/com/jeansebbeaulieu/jokes/services/JokesServiceImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class JokesServiceImpl implements JokesService {
 
-    public JokesServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public JokesServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
